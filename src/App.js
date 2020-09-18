696578b01cddd99a7e6d20116157589f8b2e9a52
praveen <praveenshahi26@gmail.com>
2020-09-18 07:24:21 +0530

    added a input field where u can enter your state

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
 import React,{useEffect, useState} from 'react';
 
 const  App=()=> {
-  const [coronadata, setcoronadata] = useState({})
+
+  const [datafetched, setdatafetched] = useState([])
+  const [state, setstate] = useState("Total");
+  const [statedata, setstatedata] = useState({})
+  let inputstate="Total";
 
   useEffect(()=>{
     fetch('https://api.covid19india.org/data.json')
     .then(res=>res.json())
-    .then(data=>{console.log(data.statewise[6]);setcoronadata(data.statewise[6])})
-  },[])
+    .then(data=>{setdatafetched(data.statewise);})
+  },[statedata])
+ 
+  const changeHandler=(e)=>{inputstate=e.target.value;}
+
+  const submitHandler=(e)=>{
+    e.preventDefault();
+    setstatedata(datafetched.filter(dataperstate=>{
+      return dataperstate.state===inputstate
+    }))
+  }
+  
   return (
     <div className="App">
-      <h1 className="title">Do U wanna see How bad the corona virus situtation in U.P </h1>
+      <h1 className="title">Do U wanna see How bad the corona virus situtation in your state is</h1>
         <div className="data">
-          <h2>active cases - {coronadata.active}</h2>
-          <h2>confirmed cases - {coronadata.confirmed}</h2>
-          <h2>deaths - {coronadata.deaths}</h2>
-          <h2>recovered - {coronadata.recovered }</h2>
+          <form onSubmit={submitHandler}>
+           Enter your state :- 
+            <input type="text" onChange={changeHandler}/>
+          </form>
+          
+          {statedata[0] && 
+          <>
+          <h1>state - {statedata[0].state}</h1>
+          <h2>active cases - {statedata[0].active}</h2>
+          <h2>Total cases - {statedata[0].confirmed}</h2>
+          <h2>Deaths - {statedata[0].deaths}</h2>
+          <h2>recovered  - {statedata[0].recovered}</h2>
+
+          {console.log(statedata[0])}
+          </>}        
+ 
         </div>
     </div>
   );
 }
 
 export default App;
+
+// ;setcoronadata(data.statewise[6])
